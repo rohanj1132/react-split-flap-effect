45f9086d27fc521345b1c00254c7b6984c88d093
Nick Anthony <nickanthony45@gmail.com>
2025-08-02 15:03:28 -0400

    fix: make sure we export FlapDisplayProvider from the library

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export { Flap } from "./Flap";
 export { FlapDigit } from "./FlapDigit";
 export { FlapDisplay } from "./FlapDisplay";
 export { FlapStack } from "./FlapStack";
+export { FlapDisplayProvider } from "./improved/FlapDisplayProvider";
 export { FlapFrame } from "./improved/FlapFrame";
 export { FlapStackImproved } from "./improved/FlapStackImproved";
 export { Presets } from "./Presets";
