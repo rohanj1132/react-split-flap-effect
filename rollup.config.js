2bbc398c24d43989e95df4003bb6f2d7d3d22598
Nick Anthony <nickanthony45@gmail.com>
2025-08-02 16:11:45 -0400

    feat: update dependencies and rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,4 @@
+import typescript from "@rollup/plugin-typescript";
 import svgr from "@svgr/rollup";
 import babel from "rollup-plugin-babel";
 import commonjs from "rollup-plugin-commonjs";
@@ -8,7 +9,7 @@ import url from "rollup-plugin-url";
 
 import postcssPresetEnv from "postcss-preset-env";
 
-import pkg from "./package.json";
+import pkg from "./package.json" assert { type: "json" };
 
 export default {
   input: "src/index.ts",
@@ -41,9 +42,16 @@ export default {
     resolve({
       extensions: [".js", ".jsx", ".ts", ".tsx"]
     }),
+    typescript({
+      tsconfig: "./tsconfig.json",
+      declaration: true,
+      declarationDir: "dist",
+      exclude: ["**/*.test.tsx", "**/*.test.ts", "src/setupTests.ts"]
+    }),
     babel({
       exclude: "node_modules/**",
-      extensions: [".js", ".jsx", ".ts", ".tsx"]
+      extensions: [".js", ".jsx"],
+      presets: ["@babel/preset-react"]
     }),
     commonjs()
   ]
